440fefc85587e49d7a4531e307f548d9331834a9
Ayo-mi <ajayijubrilayo@gmail.com>
2024-08-27 13:55:18 +0100

    google auth completed

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { ErrorBoundary, type FallbackProps } from 'react-error-boundary'
 import LoginForm from './client'
 import AuthLayout from '../auth-layout'
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 function ErrorFallback({ error }: FallbackProps) {
   return (
@@ -12,13 +13,16 @@ function ErrorFallback({ error }: FallbackProps) {
     </div>
   )
 }
+const clientId = process.env.GOOGLE_CLIENT_ID as string;
 
 export default function Page() {
   return (
+    <GoogleOAuthProvider clientId='200915400648-d5tpcs81rgjoqgac6idku39bi1dd5ga6.apps.googleusercontent.com'>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <AuthLayout showAuthLeftOptional={true}>
         <LoginForm />
       </AuthLayout>
     </ErrorBoundary>
+    </GoogleOAuthProvider>
   )
 }
