59bd6bb0b6b15c39ff40cddf3b80b85516ec1b22
emma-kingsley <159794365+emma-kingsley@users.noreply.github.com>
2024-08-31 12:08:35 +0100

    minor

--- app/(auth)/login/client.tsx
+++ app/(auth)/login/client.tsx
@@ -19,8 +19,8 @@ import { toast } from 'sonner'
 import Link from 'next/link'
 import arrow from '@/public/assets/images/arrow-right.png'
 import Image from 'next/image'
-import { GoogleLogin } from '@react-oauth/google';
-import { decodeIdToken } from '@/lib/helper-func';
+import { GoogleLogin } from '@react-oauth/google'
+import { decodeIdToken } from '@/lib/helper-func'
 
 const formSchema = z.object({
   username: z.string(),
@@ -46,34 +46,34 @@ export default function LoginForm() {
 
   const handleSuccess = async (response: any) => {
     // Extract access token from response
-    const { credential } = response;
+    const { credential } = response
     if (!credential) {
       //handle no credentials
-      return;
+      return
     }
 
     try {
-      const userInfo = decodeIdToken(credential);
-      console.log('User Info:', userInfo);
+      const userInfo = decodeIdToken(credential)
+      console.log('User Info:', userInfo)
 
       // Handle user info and perform authentication to check if user already exist in the api
     } catch (error) {
-      console.error('Error decoding ID token:', error);
+      console.error('Error decoding ID token:', error)
     }
-  };
+  }
 
   const handleError = (error: any) => {
-    console.error('Login Failed:', error);
-  };
+    console.error('Login Failed:', error)
+  }
 
   return (
-    <div className="flex flex-col gap-11">
-      <div id="login__header">
-        <h1 className="text-3xl font-semibold text-white">Login</h1>
-        <p className="text-sm"> Hi, Welcome back ✋</p>
+    <div className='flex flex-col gap-11'>
+      <div id='login__header'>
+        <h1 className='text-3xl font-semibold text-white'>Login</h1>
+        <p className='text-sm'> Hi, Welcome back ✋</p>
       </div>
-      <div className="  flex justify-between mb-4">
-        <Button className="text-[10px] font-semibold p-4 md:text-base w-[48%] bg-white flex gap-4">
+      <div className='  flex justify-between mb-4'>
+        <Button className='text-[10px] font-semibold p-4 md:text-base w-[48%] bg-white flex gap-4'>
           {/* <Image src={ether} alt="ether image" /> */}
           <Ether />
           <p>Login with Wallet</p>
@@ -85,7 +85,9 @@ export default function LoginForm() {
           onError={handleError}
           //@ts-ignore
           render={({ onClick }) => (
-            <Button onClick={onClick} className="p-4 text-[10px] font-semibold md:text-base w-[48%] bg-white flex gap-4">
+            <Button
+              onClick={onClick}
+              className='p-4 text-[10px] font-semibold md:text-base w-[48%] bg-white flex gap-4'>
               {/* <Image src={Google} alt="google image" /> */}
               <Google />
               <p>Login with Google</p>
@@ -93,31 +95,26 @@ export default function LoginForm() {
           )}
         />
       </div>
-      <div id="login__connect-wallet" className="flex flex-col gap-4">
-        <span className="flex gap-2 items-center justify-center">
-          <span className="w-[47%] h-[1px] border-[#C1C0C6] border-b-0 border-[1px]  text-[#C1C0C6]" />
-          <p className="text-[10px] font-medium text-[#C1C0C6] text-center">
-            OR
-          </p>
-          <span className="w-[47%] h-[1px] border-[#C1C0C6] border-b-0 border-[1px]" />
+      <div id='login__connect-wallet' className='flex flex-col gap-4'>
+        <span className='flex gap-2 items-center justify-center'>
+          <span className='w-[47%] h-[1px] border-[#C1C0C6] border-b-0 border-[1px]  text-[#C1C0C6]' />
+          <p className='text-[10px] font-medium text-[#C1C0C6] text-center'>OR</p>
+          <span className='w-[47%] h-[1px] border-[#C1C0C6] border-b-0 border-[1px]' />
         </span>
       </div>
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(formSubmit)}
-          className="flex flex-col gap-6"
-        >
+        <form onSubmit={form.handleSubmit(formSubmit)} className='flex flex-col gap-6'>
           <FormField
             control={form.control}
-            name="username"
+            name='username'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Username</FormLabel>
                 <FormControl {...field}>
                   <Input
-                    placeholder="Username101"
-                    className=" h-[46px] bg-transparent border-[#353538] border-solid  border-[1px] focus:border-solid focus:border-[1px] focus:border-[#353538] rounded-full"
-                    type="text"
+                    placeholder='Username101'
+                    className=' h-[46px] bg-transparent border-[#353538] border-solid  border-[1px] focus:border-solid focus:border-[1px] focus:border-[#353538] rounded-full'
+                    type='text'
                   />
                 </FormControl>
               </FormItem>
@@ -125,19 +122,19 @@ export default function LoginForm() {
           />
           <FormField
             control={form.control}
-            name="password"
+            name='password'
             render={({ field }) => (
-              <FormItem className="flex flex-col end-0">
+              <FormItem className='flex flex-col end-0'>
                 <FormLabel>Password</FormLabel>
                 <FormControl {...field}>
                   <Input
-                    placeholder="Enter your password"
-                    className=" h-[46px]  border-[#353538] border-solid  border-[1px] focus:border-solid focus:border-[1px] focus:border-[#353538] rounded-full"
-                    type="password"
+                    placeholder='Enter your password'
+                    className=' h-[46px]  border-[#353538] border-solid  border-[1px] focus:border-solid focus:border-[1px] focus:border-[#353538] rounded-full'
+                    type='password'
                   />
                 </FormControl>
-                <FormMessage className=" ml-auto w-fit">
-                  <Link href="/forgot-password" className="text-[#CFF073]">
+                <FormMessage className=' ml-auto w-fit'>
+                  <Link href='/forgot-password' className='text-[#CFF073]'>
                     Forgot Password
                   </Link>
                 </FormMessage>
@@ -145,28 +142,26 @@ export default function LoginForm() {
             )}
           />
           <Button
-            className="w-full hover:scale-105 h-[52px] text-sm font-semibold"
-            variant={'default'}
-          >
+            className='w-full hover:scale-105 h-[52px] text-sm font-semibold'
+            variant={'default'}>
             Login
           </Button>
         </form>
 
-        <div className="flex items-center justify-center w-full text-xs">
+        <div className='flex items-center justify-center w-full text-xs'>
           <p>
             {' '}
             Not registered yet?{' '}
-            <Link href="/create-account" className=" text-[#CFF073]">
+            <Link href='/create-account' className=' text-[#CFF073]'>
               Create Account
             </Link>{' '}
           </p>
-          <Image alt="arrow" src={arrow} />
+          <Image alt='arrow' src={arrow} />
         </div>
       </Form>
     </div>
   )
 }
-
 
 // {
 //   "iss": "https://accounts.google.com",
