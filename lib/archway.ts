a14ddc18e7d9f466840e4581f8a760502d9df7dd
Ayo-mi <ajayijubrilayo@gmail.com>
2024-09-19 08:56:27 +0100

    added archway

--- lib/archway.ts
+++ lib/archway.ts
@@ -1,19 +1,19 @@
 import { ArchwayClient } from '@archwayhq/arch3.js';
 
-export const getNfts = async () => {
+export const getNfts = async (ownerAddress: String) => {
     try {
         // Connect to the Archway blockchain
-        const client = await ArchwayClient.connect('https://rpc.constantine.archway.io');
+        const client = await ArchwayClient.connect('https://rpc.mainnet.archway.io');
 
         // Define contract address and owner address
         const cw721ContractAddress = 'archway1r9qqfl2ptc96frn3tx4k2n967xc64uwxg2j9xn2rvsm882fu04kq3hutsv';
-        const ownerAddress = 'archway1q73znqs6ehcgng3a8nrmz6esw2mj37d0cl4luk';
 
         // Query the smart contract
         const result = await client.queryContractSmart(cw721ContractAddress, { tokens: { owner: ownerAddress } });
 
         // Log the result
         console.log('\nQUERY RESULT:\n', result);
+
 
         // Disconnect the client
         client.disconnect();
