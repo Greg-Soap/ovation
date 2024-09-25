59f70a6e10ed3c40628418881ab04d7b681a1350
Greg-Soap <emmanuelkingsley36@gmail.com>
2024-09-25 19:49:03 +0100

    favorite is working

--- app/apps/profile/_components/favourite-nft.tsx
+++ app/apps/profile/_components/favourite-nft.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 interface FavouriteNft {
   id: string
   imageUrl: string
+  name: string
 }
 
 export default function FavouriteNft({ userId }: { userId: string }) {
@@ -25,24 +26,43 @@ export default function FavouriteNft({ userId }: { userId: string }) {
             />
           ))}
         </div>
-      ) : favouriteNfts && favouriteNfts?.data?.length > 0 ? (
+      ) : favouriteNfts && favouriteNfts?.data?.data?.length > 0 ? (
         <div className="grid grid-cols-3 gap-[6px]">
-          {favouriteNfts?.data
-            ?.slice(0, 3)
-            .map((nft: FavouriteNft) => (
+          {favouriteNfts?.data?.data?.slice(0, 3).map((nft: FavouriteNft) => (
+            <div key={nft.id} className="relative group">
               <Image
-                key={nft.id}
-                src={nft.imageUrl}
-                alt="NFT"
+                src={getImageSrc(nft.imageUrl)}
+                alt={nft.name}
                 width={100}
                 height={100}
                 className="w-full h-auto object-cover rounded-md"
               />
-            ))}
+              <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 bg-black text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                {getNFTName(nft.name)}
+              </div>
+            </div>
+          ))}
         </div>
       ) : (
         <p className="text-sm text-gray-400">No favourite NFTs found</p>
       )}
     </div>
   )
+}
+
+const getNFTName = (name: string) => {
+  if (name) return name
+
+  return 'Unnamed NFT'
+}
+
+const getImageSrc = (imageUrl: string) => {
+  if (imageUrl) {
+    if (imageUrl.startsWith('ipfs://')) {
+      return `https://gateway.pinata.cloud/ipfs/${imageUrl.slice(7)}`
+    }
+    return imageUrl
+  }
+
+  return ''
 }
