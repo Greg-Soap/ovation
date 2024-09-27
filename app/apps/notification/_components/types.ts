361f9b9f68a9135762604bfd6b88180d660f2fe5
Greg-Soap <emmanuelkingsley36@gmail.com>
2024-09-27 16:43:23 +0100

    updates

--- app/apps/notification/_components/types.ts
+++ app/apps/notification/_components/types.ts
@@ -2,11 +2,15 @@ export type NotificationType = 'Follow' | 'Badge'
 
 export interface NotificationItem {
   id: number
-  initiator?: null
+  initiator?: {
+    displayName: string
+    username: string
+    profileImage: string
+    initiatorId: string
+  }
   isFollowing: boolean
   message: string
   reference: NotificationType
   referenceId: string
   title: string
-  
 }
