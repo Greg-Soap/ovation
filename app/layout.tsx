e05773a5409cb3a6e315012c0be956cc7ff5ec5d
Greg-Soap <emmanuelkingsley36@gmail.com>
2024-10-03 16:15:25 +0100

    seo update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import './globals.css'
 import { Toaster } from '@/components/ui/sonner'
 
 export const metadata: Metadata = {
-  title: 'Ovation: Explore and Earn NFTs with Personalized Portfolios',
+  title: 'Ovation: All your NFTs – One Powerful Platform’',
   description:
-    'Discover a unique NFT marketplace where you can explore and earn NFTs through personalized portfolios. Experience the future of digital collectibles with Ovation.',
+    'Next-generation NFT social platform – Ovation is an all-in-one hub where artists, collectors, projects, and enthusiasts can discover, buy, sell, and socialize in one place designed specifically for them. Be part of the platform that is revolutionizing the future of NFTs, digital collectibles, and how we interact with them.',
   authors: [{ name: 'Emmanuel Greg' }],
   openGraph: {
-    title: 'Ovation: Explore and Earn NFTs with Personalized Portfolios',
+    title: 'Ovation: All your NFTs – One Powerful Platform’',
     description:
-      'Discover a unique NFT marketplace where you can explore and earn NFTs through personalized portfolios. Experience the future of digital collectibles with Ovation.',
+      'Next-generation NFT social platform – Ovation is an all-in-one hub where artists, collectors, projects, and enthusiasts can discover, buy, sell, and socialize in one place designed specifically for them. Be part of the platform that is revolutionizing the future of NFTs, digital collectibles, and how we interact with them.',
     images: ['https://www.ovation.network/assets/images/dashboard-demo.png'],
     siteName: 'Ovation',
     url: 'https://www.ovation.network',
@@ -18,25 +18,24 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Ovation: Explore and Earn NFTs with Personalized Portfolios',
+    title: 'Ovation: All your NFTs – One Powerful Platform’',
     description:
-      'Discover a unique NFT marketplace where you can explore and earn NFTs through personalized portfolios. Experience the future of digital collectibles with Ovation.',
+      'Next-generation NFT social platform – Ovation is an all-in-one hub where artists, collectors, projects, and enthusiasts can discover, buy, sell, and socialize in one place designed specifically for them. Be part of the platform that is revolutionizing the future of NFTs, digital collectibles, and how we interact with them.',
     creator: '@EmmanuelGreg',
     images: ['https://www.ovation.network/assets/images/dashboard-demo.png'],
   },
   icons: { shortcut: ['/favicon.ico'] },
   keywords:
-    'nft marketplace, digital collectibles, personalized portfolios, explore nfts, earn nfts',
+    '‘nft social platform, web3 social platform, nft marketplace, most expensive nft, digital collectibles, explore nfts, nft collector, nft collection, nft app, nft value, nfts for sale',
 }
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body className='overflow-x-hidden'>{children}</body>
+    <html lang="en">
+      <body className="overflow-x-hidden">{children}</body>
       <Toaster />
     </html>
   )
