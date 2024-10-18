2ca86edad1c39187c0ec8a8ff22e86091f7d233f
Greg-Soap <emmanuelkingsley36@gmail.com>
2024-10-18 11:36:16 +0100

    fix: added suspence for verify account

--- app/(auth)/verify-account/page.tsx
+++ app/(auth)/verify-account/page.tsx
@@ -3,15 +3,19 @@ import { ErrorFallback } from '@/components/error-boundary'
 import AuthLayout from '../auth-layout'
 import VerifyAccountPage from './client'
 import { ErrorBoundary } from 'react-error-boundary'
+import { Suspense } from 'react'
+import MiniLoader from '@/components/mini-loader'
 
 export default function page() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <AuthLayout>
-        <section className="flex flex-col gap-11">
-          <VerifyAccountPage />
-        </section>
-      </AuthLayout>
+      <Suspense fallback={<MiniLoader />}>
+        <AuthLayout>
+          <section className="flex flex-col gap-11">
+            <VerifyAccountPage />
+          </section>
+        </AuthLayout>
+      </Suspense>
     </ErrorBoundary>
   )
 }
