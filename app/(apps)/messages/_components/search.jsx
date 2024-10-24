f8d5c75627c371ace0ee28a707cd329d3e5f0bb2
Greg-Soap <emmanuelkingsley36@gmail.com>
2024-10-24 19:57:01 +0100

    removed popup

--- app/(apps)/messages/_components/search.jsx
+++ app/(apps)/messages/_components/search.jsx
@@ -8,19 +8,16 @@ const search = () => {
 
   return (
     <>
-      <div
-        className="mb-[26px] relative"
-        style={{ marginLeft: '26px', marginRight: '26px' }}
-      >
+      <div className='mb-[26px] relative' style={{ marginLeft: '26px', marginRight: '26px' }}>
         <SearchIcon
           size={20}
-          color="#B3B3B3"
-          className="absolute top-1/2 left-[40px] md:left-4 transform -translate-y-1/2 z-50"
+          color='#B3B3B3'
+          className='absolute top-1/2 left-[40px] md:left-4 transform -translate-y-1/2 z-50'
         />
         <input
-          type="text"
-          className="w-full border-[410.49px] pr-[47px] border-0 bg-transparent placeholder:text-[13px] placeholder:text-[#4D4D4D]"
-          placeholder="Search messages"
+          type='text'
+          className='w-full border-[410.49px] pr-[47px] border-0 bg-transparent placeholder:text-[13px] placeholder:text-[#4D4D4D]'
+          placeholder='Search messages'
           style={{
             border: '0.82px solid var(--white-15, #29292F',
             height: '44px',
@@ -30,7 +27,6 @@ const search = () => {
           }}
         />
       </div>
-      {true && <PopUp />}
     </>
   )
 }
